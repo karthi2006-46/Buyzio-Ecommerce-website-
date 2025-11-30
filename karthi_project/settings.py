d9bd0d4070ec3e450795064d8c92ec6abe9aae6b
karthi2006-46 <kartikeyan1234keyan@gmail.com>
2025-11-30 18:33:46 +0530

    Import os and fix settings

--- karthi_project/settings.py
+++ karthi_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'karthi_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import os
+
 
 DATABASES = {
     'default': {
