49f79f754b9434576c6ca567d9df6ce82a016abb
karthi2006-46 <kartikeyan1234keyan@gmail.com>
2025-11-30 18:57:16 +0530

    Use env vars for database config

--- karthi_project/settings.py
+++ karthi_project/settings.py
@@ -85,21 +85,17 @@ WSGI_APPLICATION = 'karthi_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        # 👇 Local default: 'django_karthi'
-        'NAME': os.environ.get('DB_NAME', 'django_karthi'),
-        # 👇 Local default: 'root'
-        'USER': os.environ.get('DB_USER', 'root'),
-        # 👇 Local default: your local MySQL password
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'Karthi@2006'),
-        # 👇 Local default: 'localhost'
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        # 👇 Local default: '3306'
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
 }
+
 
 
 # Password validation
