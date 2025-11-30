5e9dc02f0d7640fd6649fbb61285e597e16e0b79
karthi2006-46 <kartikeyan1234keyan@gmail.com>
2025-11-30 20:24:16 +0530

    Add product and category

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import category, Products
 
 # class CategoryAdmin(admin.ModelAdmin):
 #     list_display = ('name', 'image', 'description')  # corrected spelling
