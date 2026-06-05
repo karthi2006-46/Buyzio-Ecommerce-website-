54379ab04d84ad38842ecd3ab82900b84322b8cf
karthi2006-46 <kartikeyan1234keyan@gmail.com>
2026-06-05 12:40:52 +0530

    edit the cateory in home page

--- karthi_project/settings.py
+++ karthi_project/settings.py
@@ -117,7 +117,17 @@ USE_TZ = True
 # STATIC FILES
 # --------------------------------------------------
 STATIC_URL = "/static/"
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "django.core.files.storage.FileSystemStorage",
+#     },
+#     "staticfiles": {
+#         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+#     },
+# }
 
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "media"
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
